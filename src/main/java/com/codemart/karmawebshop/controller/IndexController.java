7de519120d10495edeed70cd5e163a7f5e27ff12
rudy42 <rus.tudor42@gmail.com>
2020-08-07 11:31:53 +0300

    contact page

--- src/main/java/com/codemart/karmawebshop/controller/IndexController.java
+++ src/main/java/com/codemart/karmawebshop/controller/IndexController.java
@@ -9,4 +9,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
 }
