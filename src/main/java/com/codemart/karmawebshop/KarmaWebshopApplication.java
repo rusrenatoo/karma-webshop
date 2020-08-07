3c35b585f626549a72685d0da0dff2a4a5409073
RusRenato <rusrenato@gmail.com>
2020-08-07 11:01:18 +0300

    init commit

---
+++ src/main/java/com/codemart/karmawebshop/KarmaWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.codemart.karmawebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarmaWebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarmaWebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codemart/karmawebshop/KarmaWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemart.karmawebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarmaWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
